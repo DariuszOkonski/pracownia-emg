799eb4095a26a1c0730c68f4d429820224948e0a
Dariusz Okonski <darek200180@gmail.com>
2020-06-15 14:40:59 +0100

    add btnOpen

--- src/js/another.js
+++ src/js/another.js
@@ -1,1 +1,19 @@
-console.log('hello another');
+uiQuery = {
+    dataButtonHamburgerOpen: "[data-button-hamburger-open]"
+}
+
+uiElements = {
+    btnOpen: document.querySelector(uiQuery.dataButtonHamburgerOpen)
+}
+
+const addEventListeners = () => {
+    uiElements.btnOpen.addEventListener('click', () => openMobileMenu())
+}
+
+addEventListeners();
+
+// additional methods =========================
+const openMobileMenu = () => {
+    alert('openMobileMenu');
+}
+
