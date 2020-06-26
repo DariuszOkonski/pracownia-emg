2683359f1ace1acdc9a90b0cd20cd7ec3ea383a9
Dariusz Okonski <darek200180@gmail.com>
2020-06-26 13:53:40 +0100

    import AOS library and set basic settings

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,14 @@ import '../assets/lightbox-dist/css/lightbox.css';
 import '../scss/main.scss';
 
 console.log('HELLO 🚀')
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+    startEvent: 'DOMContentLoaded',
+    duration: 1000,
+    offset: 100,
+    delay: 200,
+    once: true
+});
